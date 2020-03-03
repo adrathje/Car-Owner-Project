f798fdd6c3dcde0b7852f6d38fb73858d594064f
Austin Rathje <austinrathje@gmail.com>
2020-03-02 22:55:49 -0600

    Populated entities
    Car, CarOrder, and Person entities.

--- src/model/Car.java
+++ src/model/Car.java
@@ -1,6 +1,117 @@
 package model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "car")
 public class Car {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "CAR_ID")
+	private int carID;
+	@Column(name = "MODEL")
+	private String model;
+	@Column(name = "MAKE")
+	private String make;
+	@Column(name = "TYPE")
+	private String type;
+	
+	
+	/**
+	 * default constructor, takes no arguments
+	 */
+	public Car() {
+	}
 
+	/**
+	 * @param model
+	 * @param make
+	 * @param type
+	 */
+	public Car(String model, String make, String type) {
+		this.model = model;
+		this.make = make;
+		this.type = type;
+	}
+
+	/**
+	 * @param carID
+	 * @param model
+	 * @param make
+	 * @param type
+	 */
+	public Car(int carID, String model, String make, String type) {
+		this.carID = carID;
+		this.model = model;
+		this.make = make;
+		this.type = type;
+	}
+
+	/**
+	 * @return the carID
+	 */
+	public int getCarID() {
+		return carID;
+	}
+
+	/**
+	 * @param carID the carID to set
+	 */
+	public void setCarID(int carID) {
+		this.carID = carID;
+	}
+
+	/**
+	 * @return the model
+	 */
+	public String getModel() {
+		return model;
+	}
+
+	/**
+	 * @param model the model to set
+	 */
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	/**
+	 * @return the make
+	 */
+	public String getMake() {
+		return make;
+	}
+
+	/**
+	 * @param make the make to set
+	 */
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return "Car [carID=" + carID + ", model=" + model + ", make=" + make + ", type=" + type + "]";
+	}
+	
+	
 }
--- src/model/Person.java
+++ src/model/Person.java
@@ -1,5 +1,87 @@
 package model;
 
-public class Person {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "person")
+public class Person {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "PERSON_ID")
+	private int personID;
+	@Column(name = "FIRST_NAME")
+	private String firstName;
+	@Column(name = "LAST_NAME")
+	private String lastName;
+	
+	/**
+	 * default constructor, takes no arguments
+	 */
+	public Person() {
+	}
+	/**
+	 * @param firstName
+	 * @param lastName
+	 */
+	public Person(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	/**
+	 * @param personID
+	 * @param firstName
+	 * @param lastName
+	 */
+	public Person(int personID, String firstName, String lastName) {
+		this.personID = personID;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	/**
+	 * @return the personID
+	 */
+	public int getPersonID() {
+		return personID;
+	}
+	/**
+	 * @param personID the personID to set
+	 */
+	public void setPersonID(int personID) {
+		this.personID = personID;
+	}
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	@Override
+	public String toString() {
+		return "Person [personID=" + personID + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+	
+	
 }
